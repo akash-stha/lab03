2c60a2dbb37b706a939d4e7256ea9122ed4ba4eb
Akash Shrestha <aakashtha49@gmail.com>
2023-12-09 20:09:55 -0500

    new storyboards added

---
+++ Lab03/CityListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityListTableViewCell.swift
+//  Lab03
+//
+//  Created by Akash Shrestha on 2023-12-09.
+//
+
+import UIKit
+
+class CityListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
